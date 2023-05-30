2e5bcaee70cc06ebd9297bc0b856b6086709e240
Autistnic <christian.alingasa@outlook.com>
2023-05-30 10:30:30 +0800

    portfolio v1.2

--- src/components/Sidenav.tsx
+++ src/components/Sidenav.tsx
@@ -13,19 +13,19 @@ function Sidenav() {
   {
     nav ? (
 <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
-<a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200'>
+<a onClick={handleNav} href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200'>
     <AiOutlineHome size={20}/>
     <span className='pl-4'>Home</span>
 </a>
-<a href="#about" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200'>
+<a onClick={handleNav} href="#about" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200'>
     <VscAccount size={20}/>
     <span className='pl-4'>About</span>
 </a>
-<a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200'>
+<a onClick={handleNav} href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200'>
     <ImFileText2 size={20}/>
     <span className='pl-4'>Resume</span>
 </a>
-<a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200'>
+<a onClick={handleNav} href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200'>
     <AiOutlineMail size={20}/>
     <span className='pl-4'>Contact</span>
 </a>
