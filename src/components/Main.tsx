a92fe50f5217a825c1c8aa436891a19ee54d8dfe
Autistnic <christian.alingasa@outlook.com>
2023-05-30 10:58:22 +0800

    portfolio v0.1

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,10 +8,10 @@ function Main() {
 <div className='w-full h-screen absolute top-0 left-0 bg-white/30'>
   <div className='max-w-[700px] pr-[90] pb-[250px] h-full w-full flex flex-col justify-center lg:items-start items-center lg:pl-[100px]'><h1 className=' text-4xl font-bold text-gray-800 flex'>
     I'm </h1>
-    <h1 className=' text-4xl font-bold text-gray-800 flex text-center'>Christian Marc Nico Alingasa</h1>
+    <h1 className=' text-4xl font-bold text-gray-800 flex text-center'>Onell Murphy</h1>
     <h2 className='flex sm:text-3xl text-2xl pt-2 text-gray-800'>I'm a <TypeAnimation
       sequence={[
-        'Developer',
+        'Palautog',
         2000,
         'Gamer',
         2000,
