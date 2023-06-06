ef022d93767b5e25c3520eadfa0fcd2726132a52
Autistnic <christian.alingasa@outlook.com>
2023-06-06 08:40:22 +0800

    Portfolio v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import Main from './components/Main'
-import Sidenav from './components/Sidenav'
-import About from './components/About'
 function App() {
 
   return (
     <div>
-<Sidenav/>
 <Main/>
-<About/>
     </div>
   )
 }
